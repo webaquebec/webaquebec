aa340a6df9cc033d443757be4b9e534625b3109b
lornz- <lrt.hrl@gmail.com>
2021-02-07 23:46:04 -0500

    :wrench:  Add/setup DM Sans google webfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,24 @@ module.exports = {
       },
       __key: 'images',
     },
+    {
+      /**
+       * A Gatsby plugin to handle cdn, base64 and self hosted webfonts
+       * see https://www.gatsbyjs.com/plugins/gatsby-plugin-webfonts/?=gatsby-plugin-webfonts
+       */
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: 'DM Sans',
+              variants: ['400', '500', '700'],
+              fontDisplay: 'swap',
+              strategy: 'selfHosted', // 'base64' || 'cdn'
+            },
+          ],
+        },
+      },
+    },
   ],
 };
