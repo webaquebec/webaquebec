b50b7e0495d5396e4ab23a7caddc30b9fe2bfde3
lornz- <lrt.hrl@gmail.com>
2021-02-07 23:57:30 -0500

    :art:  Add interpolate function

---
+++ src/utils/math/interpolate.js
@@ -0,0 +1,11 @@
+/**
+ * Computes a CSS calc function that betweens a value from
+ * A to B over A-viewport-width to B-viewport-width.
+ * Requires a media query to cap the value at B.
+ */
+export default (from, to, fromWidth, toWidth) => {
+  const slope = (to - from) / (toWidth - fromWidth);
+  const base = to - slope * fromWidth - from;
+
+  return `calc(1rem + ${100 * slope}vw + ${base}px)`;
+};
