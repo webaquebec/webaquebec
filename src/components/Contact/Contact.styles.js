787912728827930cab0dcc1ceef09e9e58c9ae56
Krystina <krystina.hamel@libeo.com>
2021-02-28 12:31:10 -0500

    :construction:  Intégration des blocs de contact

---
+++ src/components/Contact/Contact.styles.js
@@ -0,0 +1,58 @@
+// vendors
+import styled, { css } from 'styled-components';
+
+// styles
+import colors from '../../styles/colors';
+import breakpoints from '../../styles/breakpoints';
+import breakpointsRange from '../../utils/breakpointsRange';
+
+export const ContactTitle = styled.h2`
+  margin: 0;
+
+  text-align: center;
+
+  ${breakpointsRange(
+    [{ prop: 'fontSize', sizes: [18, 24], bases: [16, 20] }],
+    breakpoints.spacings
+  )};
+`;
+
+export const ContactText = styled.p`
+  text-align: center;
+
+  ${breakpointsRange(
+    [{ prop: 'fontSize', sizes: [14, 16], bases: [16, 20] }],
+    breakpoints.spacings
+  )};
+`;
+
+export const MediaText = css`
+  color: ${colors.gris90};
+`;
+
+export const MediaList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: auto 0 0;
+  padding: 0;
+
+  list-style: none;
+`;
+
+export const BlocContainer = css`
+  ${breakpointsRange(
+    [
+      { prop: 'paddingTop', sizes: [32, 80] },
+      { prop: 'paddingRight', sizes: [32, 80] },
+      { prop: 'paddingBottom', sizes: [32, 80] },
+      { prop: 'paddingLeft', sizes: [32, 80] },
+    ],
+    breakpoints.spacings,
+    { bases: [16, 20] }
+  )};
+`;
+
+export const ContentContainer = css`
+  height: 100%;
+`;
