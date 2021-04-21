99d5cc981e18f8205a572367f377f2e04725b40b
lornz- <lrt.hrl@gmail.com>
2021-04-21 15:06:51 -0400

    :sparkles:  Create CardGrid/Card components

---
+++ src/images/IconArrow.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const IconArrow = (props) => (
+  <svg
+    viewBox='0 0 29 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M2 12L26 12'
+      stroke='currentColor'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M17 22L27 12L17 2'
+      stroke='currentColor'
+      strokeWidth='3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export default IconArrow;
