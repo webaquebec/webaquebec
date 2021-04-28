3fcce142f3b12ad24b62156990c10c373d429532
lornz- <lrt.hrl@gmail.com>
2021-04-28 17:11:41 -0400

    :chart_with_upwards_trend:  Replace gatsby google gtag plugin
    Adjust gatsby plugins order declaration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,43 @@ module.exports = {
     siteUrl: 'https://www.webaquebec.org',
   },
   plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-KHD8ZJR',
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-20043510-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: '758649984324647',
+      },
+    },
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -37,66 +70,6 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {},
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          'UA-20043510-1', // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: 'OPT_CONTAINER_ID',
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Gtag will not be loaded at all for visitors that have "Do Not Track" enabled
-          respectDNT: true,
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-google-tagmanager',
-      options: {
-        id: 'GTM-KHD8ZJR',
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: 'gatsby' },
-
-        // Specify optional GTM environment details.
-        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        // dataLayerName: 'YOUR_DATA_LAYER_NAME',
-
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        // routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: '758649984324647',
-      },
-    },
+    `gatsby-plugin-sitemap`,
   ],
 };
