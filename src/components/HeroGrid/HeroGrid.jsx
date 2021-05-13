5bbc4ba699df895c5a89210d2a670b471c4c399c
Krystina <krystina.hamel@libeo.com>
2021-05-13 17:40:59 -0400

    :art:  Separate title from hero grid

--- src/components/HeroGrid/HeroGrid.jsx
+++ src/components/HeroGrid/HeroGrid.jsx
@@ -1,39 +1,23 @@
 // vendors
 import React from 'react';
 import PropTypes from 'prop-types';
-import { hideVisually } from 'polished';
 
 // components
 import Center from '../LayoutSections/Center';
+import Hero from '../Hero';
 
 // images
-import vectorYear2021 from '../../images/vectorYear2021.svg';
 import vectorHeroGrid from '../../images/vectorHeroGrid.svg';
 
 // styles
-import {
-  GridWrapper,
-  GridVector,
-  GridContent,
-  GridTitle,
-  GridTitleWrapper,
-  YearSticker,
-} from './HeroGrid.styles';
+import { GridWrapper, GridVector, GridContent } from './HeroGrid.styles';
 
 const HeroGrid = ({ title, displayYear }) => {
   return (
     <Center maxWidth='1080px' css={GridWrapper}>
       <GridVector src={vectorHeroGrid} alt='' role='presentation' />
       <GridContent>
-        <GridTitleWrapper>
-          <GridTitle>
-            {title}
-            {displayYear && <span css={hideVisually}>2021</span>}
-          </GridTitle>
-          {displayYear && (
-            <YearSticker src={vectorYear2021} alt='' role='presentation' />
-          )}
-        </GridTitleWrapper>
+        <Hero title={title} displayYear={displayYear} />
       </GridContent>
     </Center>
   );
--- src/components/HeroGrid/HeroGrid.styles.js
+++ src/components/HeroGrid/HeroGrid.styles.js
@@ -7,8 +7,6 @@ import { lessThan } from '../../utils/mediaQuery';
 
 // styles
 import breakpoints from '../../styles/breakpoints';
-import colors from '../../styles/colors';
-import { fontFamilies, fontWeights } from '../../styles/typography';
 
 export const GridWrapper = css`
   position: relative;
@@ -39,58 +37,6 @@ export const GridContent = styled.div`
     [
       { prop: 'paddingTop', sizes: [60, 120], bases: [16, 20] },
       { prop: 'paddingBottom', sizes: [60, 120], bases: [16, 20] },
-    ],
-    breakpoints.spacings
-  )};
-`;
-
-export const GridTitle = styled.h1`
-  margin: 0;
-
-  color: ${colors.bleu80};
-
-  font-weight: ${fontWeights.bold};
-  font-family: ${fontFamilies.redaction35};
-
-  ${breakpointsRange(
-    [
-      { prop: 'fontSize', sizes: [32, 96], bases: [16, 20] },
-      { prop: 'lineHeight', sizes: [38, 116], bases: [16, 20] },
-    ],
-    breakpoints.spacings
-  )};
-`;
-
-export const GridTitleWrapper = styled.div`
-  position: relative;
-
-  display: inline-block;
-
-  background-color: ${colors.gris30};
-  border: 3px solid ${colors.bleu80};
-  transform: rotate3d(0, 0, 1, 3.82deg);
-
-  ${breakpointsRange(
-    [
-      { prop: 'borderRadius', sizes: [12, 16], bases: [16, 20] },
-      { prop: 'paddingTop', sizes: [12, 24], bases: [16, 20] },
-      { prop: 'paddingBottom', sizes: [12, 24], bases: [16, 20] },
-      { prop: 'paddingLeft', sizes: [24, 40], bases: [16, 20] },
-      { prop: 'paddingRight', sizes: [24, 40], bases: [16, 20] },
-    ],
-    breakpoints.spacings
-  )};
-`;
-
-export const YearSticker = styled.img`
-  position: absolute;
-
-  transform: translateY(100%) rotate3d(0, 0, 1, -5.09deg);
-
-  ${breakpointsRange(
-    [
-      { prop: 'width', sizes: [100, 200], bases: [16, 20] },
-      { prop: 'bottom', sizes: [16, 32], bases: [16, 20] },
     ],
     breakpoints.spacings
   )};
