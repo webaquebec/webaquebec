139bd05f60537d5d2bcce5670fe3563940face59
lornz- <lrt.hrl@gmail.com>
2021-05-25 09:23:53 -0400

    :mag:  Change meta title (Program)

--- src/templates/Program/Program.jsx
+++ src/templates/Program/Program.jsx
@@ -85,7 +85,7 @@ const Program = ({
   return (
     <Layout location={location}>
       <SEO
-        title='Programmation — Le Web à Québec'
+        title='Programmation'
         description='Plus de 50 conférences sur 3 jours avec des ateliers, du réseautage et une multitude d’activités. Découvre la programmation du Web à Québec.'
       />
 
