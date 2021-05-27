a11de670d7ce0e59d3a933c5ca13017330f8c136
lornz- <lrt.hrl@gmail.com>
2021-05-27 15:16:52 -0400

    :speech_balloon:  Add FIXME note (Paper)

--- src/components/Paper/Paper.styles.js
+++ src/components/Paper/Paper.styles.js
@@ -34,6 +34,8 @@ const Container = styled(Box)`
 
     border-radius: inherit;
 
+    /* FIXME: This is hell. Find another way if a drop-shadow is required. */
+
     /* filter: drop-shadow(0 30px 80px rgba(0, 12, 158, 0.5)); */
 
     mix-blend-mode: normal;
