f0c10cf78920699e86dab80127a0330018ec883d
lornz- <lrt.hrl@gmail.com>
2021-05-28 22:42:22 -0400

    :art:  Move out data mapping in one utils file

---
+++ src/utils/dataMapping.js
@@ -0,0 +1,17 @@
+export const categoriesMap = {
+  design: 'Design',
+  developpement: 'Développement',
+  innovation: 'Innovation',
+  'communication-et-marketing': 'Communication & marketing',
+};
+
+// Event type tag
+export const eventTypesMap = {
+  conference: 'Conférence',
+  atelier: 'Atelier',
+  reseautage: 'Réseautage',
+  qanda: 'Q&A',
+  keynote: 'Keynote',
+  'contenu-sur-demande': 'Contenu sur demande',
+  'pitch-ton-waq': 'Pitch ton WAQ',
+};
