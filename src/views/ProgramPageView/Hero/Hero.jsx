b04f4643a604d5f61301d0e7c3d24b0aa31bf550
lornz- <lrt.hrl@gmail.com>
2021-05-31 16:04:05 -0400

    :art:  Handle react rehydration issue

--- src/views/ProgramPageView/Hero/Hero.jsx
+++ src/views/ProgramPageView/Hero/Hero.jsx
@@ -2,11 +2,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useInView } from 'react-intersection-observer';
+import { css } from 'styled-components';
 
 // components
 import HeroGrid from '../../../components/HeroGrid/HeroGrid';
 import HeaderGradient from '../../../components/HeaderGradient/HeaderGradient';
 import Button from '../../../components/Button';
+
+// hooks
+import useHasMounted from '../../../hooks/useHasMounted';
 
 // images
 import vectorYear2021 from '../../../images/vectorYear2021.svg';
@@ -22,42 +26,51 @@ import {
   dateTabStyle,
 } from './Hero.styles';
 
+const willChangeOpacityStyle = css`
+  will-change: opacity;
+`;
+
 const Hero = ({ datePaths }) => {
-  const [ref, inView] = useInView({
-    rootMargin: '0px',
-  });
+  const [ref, inView] = useInView();
+
+  const hasMounted = useHasMounted();
 
   return (
     <>
       <HeroGrid ref={ref} title='programmation' displayYear />
 
       <Wrapper>
-        <HeaderGradient visible={!inView} />
+        <HeaderGradient
+          css={willChangeOpacityStyle}
+          style={{ opacity: hasMounted && !inView ? 1 : 0 }}
+        />
 
-        <HeaderContent maxWidth={!inView ? '1066px' : '736px'}>
-          {!inView && (
-            <StickyTitle>
-              programmation
-              <YearSticker src={vectorYear2021} alt='2021' />
-            </StickyTitle>
-          )}
-          <DateList>
-            {datePaths.map((item) => (
-              <DateListItem key={item.date}>
-                <Button
-                  to={item.path}
-                  activeClassName='active'
-                  outlined
-                  medium
-                  tag='link'
-                  css={dateTabStyle}
-                >
-                  <span>{item.date}</span>
-                </Button>
-              </DateListItem>
-            ))}
-          </DateList>
-        </HeaderContent>
+        {hasMounted && (
+          <HeaderContent maxWidth={!inView ? '1066px' : '736px'}>
+            {!inView && (
+              <StickyTitle>
+                programmation
+                <YearSticker src={vectorYear2021} alt='2021' />
+              </StickyTitle>
+            )}
+            <DateList>
+              {datePaths.map((item) => (
+                <DateListItem key={item.date}>
+                  <Button
+                    to={item.path}
+                    activeClassName='active'
+                    outlined
+                    medium
+                    tag='link'
+                    css={dateTabStyle}
+                  >
+                    <span>{item.date}</span>
+                  </Button>
+                </DateListItem>
+              ))}
+            </DateList>
+          </HeaderContent>
+        )}
       </Wrapper>
     </>
   );
