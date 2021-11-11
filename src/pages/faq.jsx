688bb8f041f0f6a0f59920cc7da47111230047be
lornz- <lrt.hrl@gmail.com>
2021-11-11 14:25:49 -0500

    :speech_balloon:  Update faq

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -62,69 +62,40 @@ const faqItem = css`
     ],
     breakpoints.spacings
   )};
+
+  a {
+    color: ${colors.bleu};
+  }
 `;
 
 const FaqPage = () => {
   const data = [
     {
       id: 1,
-      question: 'Quelle sera la forme du WAQ21?',
+      question: 'Quelle sera la forme du WAQ22?',
       answer:
-        'Du 15 au 17 juin prochains, le WAQ21 aura lieu virtuellement. On te convie à une édition « pas comme les autres »  sur la plateforme SwapCard. Les détenteurs de billets pour l’édition 2020 du Web à Québec recevront plus d’informations par courriel.',
+        "Le WAQ se tiendra du 24 au 26 mai 2022. Le format de l’événement sera annoncé dans les prochaines semaines (mais sois rassuré·e, c’est des bonnes nouvelles qui t’attendent!). D’ailleurs, les billets seront en vente au début de l’année 2022. Ouvre l'œil!",
     },
     {
       id: 2,
-      question: 'Est-ce que mon billet pour le WAQ20 est remboursable?',
+      question:
+        'Comment est-ce que je peux utiliser mon crédit de l’édition 2020-2021?',
       answer:
-        'Les billets sont non remboursables puisqu’il s’agit d’une vente finale. Ils peuvent cependant être transférés à une autre personne.',
+        "Une communication par courriel te sera envoyée au moment de la mise en vente des billets. La procédure complète y sera expliquée. Si jamais tu ne reçois rien de notre part, tu peux nous écrire à <a href='mailto:info@webaquebec.org'>info@webaquebec.org</a>. Les crédits seront applicables uniquement sur les billets de cette édition-ci (WAQ22).",
     },
     {
       id: 3,
       question:
-        'Est-il possible de transférer un billet du WAQ à une autre personne?',
+        'Est-ce possible de participer à la Semaine numériQC avec un billet du WAQ?',
       answer:
-        'Les transferts de billet d’une personne à une autre sont possibles. Écris-nous à <a href="mailto:info@webaquebec.org">info@webaquebec.org</a> pour connaître la marche à suivre.',
+        'Non. Il s’agit de deux événements distincts, donc ça prend un billet pour la SNQC et un billet pour le WAQ.',
     },
     {
       id: 4,
       question:
-        'Si je possède un billet WAQ20, est-il encore valide? Qu’est-ce qu’il comprend?',
+        'Est-ce qu’il y a des chances que le WAQ22 soit annulé ou reporté?',
       answer:
-        'Oui. Tous les détenteurs de billets pour l’édition prévue en 2020 du Web à Québec auront accès à l’édition de 2021 qui se tiendra du 15 au 17 juin prochains. L’édition 2021 sera virtuelle, tu n’as aucune action à prendre pour transférer ton billet, le tout se fait automatiquement. Tu recevras également un code qui te permet d’accéder à deux accès supplémentaires pour le Web à Québec. Deux accès que nous t’invitons à donner à des ami.es et/ou collègues de la communauté numérique. Finalement, nous t’offrons également un crédit applicable sur l’achat d’un billet pour l’édition 2022. Ne t’inquiète pas, tous les détails te seront envoyés par courriel.',
-    },
-    {
-      id: 5,
-      question:
-        'Il me sera impossible d’assister à l’événement en juin. Est-ce que je peux me faire rembourser?',
-      answer:
-        'Les billets sont non remboursables puisqu’il s’agit d’une vente finale. Ils peuvent cependant être transférés à une autre personne. Dans le cas où un transfert n’est pas possible, un crédit sur l’achat d’un billet pour l’édition 2022 vous sera offert.',
-    },
-    {
-      id: 6,
-      question:
-        'Est-ce possible de participer à la Semaine numériQC avec un billet du WAQ?',
-      answer:
-        'Non. Les seules personnes qui peuvent assister aux deux événements sont celles qui se sont procurées une passe all-access l’année dernière. Des billets seront cependant mis en vente pour la <a rel="noopener noreferrer" target="_blank" href="https://www.semainenumeriqc.com/" target="_blank" rel="noopener noreferrer">Semaine numériQC</a> dans les mois à venir.',
-    },
-    {
-      id: 7,
-      question:
-        'Est-ce qu’il y a des chances que le WAQ21 soit annulé ou reporté de nouveau?',
-      answer:
-        'L’équipe du Web à Québec se donne comme mission de présenter une édition 2021 du WAQ en juin, peu importe la situation. Que l’événement soit 100% virtuel ou hybride, nous nous engageons à te livrer une expérience aux couleurs du WAQ, à moins qu’une situation hors de notre contrôle force l’annulation.',
-    },
-    {
-      id: 8,
-      question:
-        'Est-ce que des billets supplémentaires seront mis en vente pour participer à l’édition 2021 du WAQ?',
-      answer: 'Non, l’événement affiche complet depuis février 2020!',
-    },
-    {
-      id: 9,
-      question:
-        'Puis-je me procurer des billets pour l’édition virtuelle de 2021?',
-      answer:
-        'Non, il n’y aura pas de nouveaux billets en vente. Si tu souhaites participer au WAQ21, nous offrons deux billets supplémentaires par billet acheté pour l’édition 2020. Dépêche-toi de solliciter ton réseau de contacts pour obtenir un laissez-passer pour 2021.',
+        'Comme on n’a pas de boule de cristal, il est impossible de le confirmer. PAR CONTRE, on peut confirmer que l’événement aura lieu, peu importe la situation.',
     },
   ];
 
@@ -140,10 +111,9 @@ const FaqPage = () => {
       >
         <FaqTitle css={h1AltStyle}>FAQ</FaqTitle>
         <FaqIntro css={introStyle}>
-          L’édition 2021 du Web à Québec (WAQ) est reportée aux 15, 16 et 17
-          juin prochains. Si vous êtes l’un.e des fier.ères détenteur.trices de
-          billet du WAQ ou si vous avez des questions sur l’événement et/ou sa
-          forme, on tente d’y répondre ici. 👇
+          L’édition 2022 du Web à Québec (WAQ) aura lieu les 24, 25 et 26 mai
+          prochains. Si vous avez des questions sur l’événement et/ou sa forme,
+          on tente d’y répondre ici! 👇
         </FaqIntro>
       </Center>
 
