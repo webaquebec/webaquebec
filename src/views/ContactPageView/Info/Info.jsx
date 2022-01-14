462055ba0c76c2f6243e725fd5d6a206d4b68142
florianfmmartin <florian.fm.martin@gmail.com>
2022-01-14 10:48:30 -0500

    :construction: (page-contact) Section d'informations de la page contact
    Presque complété, il manque le lien pour le boutton et d'obtenir une image sans bordure.

--- src/views/ContactPageView/Info/Info.jsx
+++ src/views/ContactPageView/Info/Info.jsx
@@ -1,6 +1,55 @@
 // vendors
 import React from 'react';
 
-const Info = () => <div />;
+// components
+import Center from '../../../components/LayoutSections/Center';
+import Sidebar from '../../../components/LayoutSections/Sidebar/Sidebar';
+import Button from '../../../components/Button';
+
+// styles
+import {
+  Img,
+  ImgContainer,
+  SectionContainer,
+  Text,
+  TextContainer,
+  Title,
+} from './Info.styles';
+
+// images
+import imgQuebecNumerique from '../../../images/img-quebec-numerique.jpg';
+
+const Info = () => (
+  <SectionContainer>
+    <Center maxWidth='1064px' gutters='var(--container-gutter)'>
+      <Sidebar contentMin='50%' sideWidth='531px' side='left' space='0'>
+        <div>
+          <ImgContainer>
+            <Img
+              src={imgQuebecNumerique}
+              alt='Les membres de Québec Numérique'
+            />
+          </ImgContainer>
+          <div>
+            <TextContainer>
+              <Title>Québec Numérique</Title>
+              <Text>
+                Texte standard lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam.
+              </Text>
+              {
+                // FIXME: Fix the link
+              }
+              <Button to='/' tag='href' primary small animated>
+                plus d&apos;information
+              </Button>
+            </TextContainer>
+          </div>
+        </div>
+      </Sidebar>
+    </Center>
+  </SectionContainer>
+);
 
 export default Info;
---
+++ src/views/ContactPageView/Info/Info.styles.js
@@ -0,0 +1,62 @@
+// vendors
+import styled from 'styled-components';
+
+// utils
+import breakpointsRange from '../../../utils/breakpointsRange';
+
+// styles
+import breakpoints from '../../../styles/breakpoints';
+import { h4Style, introStyle } from '../../../styles/global';
+import colors from '../../../styles/colors';
+
+export const SectionContainer = styled.div`
+  ${breakpointsRange(
+    [
+      { prop: 'marginTop', sizes: [32, 62], bases: [16, 20] },
+      { prop: 'marginBottom', sizes: [24, 57], bases: [16, 20] },
+    ],
+    breakpoints.spacings
+  )};
+`;
+
+export const TextContainer = styled.div`
+  ${breakpointsRange(
+    [{ prop: 'marginLeft', sizes: [71, 107], bases: [16, 20] }],
+    breakpoints.spacings
+  )};
+
+  max-width: 426px;
+`;
+
+export const ImgContainer = styled.div`
+  object-fit: contain;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  ${h4Style}
+  color: ${colors.bleu80};
+  ${breakpointsRange(
+    [
+      { prop: 'marginTop', sizes: [24, 70], bases: [16, 20] },
+      { prop: 'marginBottom', sizes: [8, 16], bases: [16, 20] },
+    ],
+    breakpoints.spacings
+  )};
+`;
+
+export const Text = styled.p`
+  ${introStyle}
+
+  margin-top: 0;
+
+  color: ${colors.gris100};
+
+  ${breakpointsRange(
+    [{ prop: 'marginBottom', sizes: [24, 56], bases: [16, 20] }],
+    breakpoints.spacings
+  )};
+`;
