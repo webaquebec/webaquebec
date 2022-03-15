cc57311955abc3baeddaa29d664bae4efeb82022
lornz- <lrt.hrl@gmail.com>
2022-03-15 14:53:37 -0400

    :art:  Update blog post read also section view
    Handle data from CMS
    
    Adjust layout and styles

--- src/views/BlogPostView/ReadAlsoSection/ReadAlsoSection.jsx
+++ src/views/BlogPostView/ReadAlsoSection/ReadAlsoSection.jsx
@@ -1,113 +1,99 @@
 // vendors
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // styles
-import { Title, Container } from './ReadAlsoSection.styles';
+import { Title, Star } from './ReadAlsoSection.styles';
 
 // components
+import Center from '../../../components/LayoutSections/Center';
 import CardGrid from '../../../components/CardGrid';
 import Card from '../../../components/CardGrid/Card';
 
-const ReadAlsoSection = () => {
-  const data = [
-    {
-      title: 'Le WAQ a besoin de toi comme bénévole!',
-      date: '6 avril 2021',
-      content:
-        'Coutre intro lorem ipsum dolor sit amet, consectetur adipiscing elit, sed duom...',
-      picture: {
-        childImageSharp: {
-          desktop: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-          mobile: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-        },
-      },
-      to: '/',
-    },
-    {
-      title:
-        'Le WAQ a besoin de toi comme bénévole! Le WAQ a besoin de toi comme bénévole!',
-      date: '6 avril 2021',
-      content:
-        'Coutre intro lorem ipsum dolor sit amet, consectetur adipiscing elit, sed duom...',
-      picture: {
-        childImageSharp: {
-          desktop: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-          mobile: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-        },
-      },
-      to: '/',
-    },
-    {
-      title: 'Le WAQ a besoin de toi comme bénévole!',
-      date: '6 avril 2021',
-      content:
-        'Coutre intro lorem ipsum dolor sit amet, consectetur adipiscing elit, sed duom...',
-      picture: {
-        childImageSharp: {
-          desktop: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-          mobile: {
-            aspectRatio: 1.89,
-            src: 'https://via.placeholder.com/380x200',
-            srcSet: '',
-            srcSetWebp: '',
-            srcWebp: '',
-          },
-        },
-      },
-      to: '/',
-    },
-  ];
+const ReadAlsoSection = ({ featuredPosts }) => {
+  const posts = featuredPosts.map(({ node }) => {
+    const pictures = node.featuredImage?.node?.localFile?.childImageSharp;
+
+    return {
+      ...node,
+      pictures,
+      to: `/blogue/${node.slug}`,
+    };
+  });
+
   return (
-    <Container
+    <Center
       maxWidth='var(--max-container-width)'
       gutters='var(--container-gutter)'
+      css={`
+        width: calc(100% - 2 * var(--container-gutter));
+      `}
     >
       <Title>À lire également</Title>
+
       <CardGrid>
-        {data.map((item) => (
-          <Card
-            title={item.title}
-            titleAs='h3'
-            date={item.date}
-            content={item.content}
-            picture={item.picture}
-            to={item.to}
-            buttonText='Lire l’article'
-          />
-        ))}
+        {posts.map((item) => {
+          const picture = {
+            ...item.pictures?.featuredSmall,
+            sizes: `
+            (min-width: 576px) ${(695 / 734) * 100}vw,
+            (min-width: 734px) ${(516 / 1138) * 100}vw,
+            (min-width: 1138px) ${(378 / 1280) * 100}vw,
+            (min-width: 1280px) 378px,
+            ${(538 / 576) * 100}vw`,
+          };
+
+          return (
+            <Card
+              key={item.id}
+              title={item.title}
+              titleAs='h3'
+              complement={
+                <>
+                  <Star /> {item.date}
+                </>
+              }
+              content={item.excerpt}
+              picture={picture}
+              to={item.to}
+              buttonText="Lire l'article"
+            />
+          );
+        })}
       </CardGrid>
-    </Container>
+    </Center>
   );
 };
 
 export default ReadAlsoSection;
+
+ReadAlsoSection.propTypes = {
+  featuredPosts: PropTypes.arrayOf(
+    PropTypes.shape({
+      node: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        excerpt: PropTypes.string.isRequired,
+        slug: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        featuredImage: PropTypes.shape({
+          node: PropTypes.shape({
+            localFile: PropTypes.shape({
+              childImageSharp: PropTypes.shape({
+                featuredSmall: PropTypes.shape({
+                  src: PropTypes.string.isRequired,
+                  srcSet: PropTypes.string.isRequired,
+                  srcWebp: PropTypes.string.isRequired,
+                  srcSetWebp: PropTypes.string.isRequired,
+                  base64: PropTypes.string.isRequired,
+                  aspectRatio: PropTypes.number.isRequired,
+                  sizes: PropTypes.string.isRequired,
+                }).isRequired,
+              }),
+            }),
+          }),
+        }),
+      }),
+    })
+  ).isRequired,
+};
