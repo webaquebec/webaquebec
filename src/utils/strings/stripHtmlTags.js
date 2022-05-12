535f49254884178466ae46b82f36c75f7df29663
lornz- <lrt.hrl@gmail.com>
2022-05-12 13:10:23 -0400

    :bug: (blogpost) Fix links displaying into paragraph blocks

--- src/utils/strings/stripHtmlTags.js
+++ src/utils/strings/stripHtmlTags.js
@@ -1,1 +1,10 @@
-export default (string) => string.replace(/(<([^>]+)>)/gi, '');
+/**
+ * Strip all HTML tags or from a list of restricted tags into a string
+ */
+export default (string, options = { unwantedTags: [] }) => {
+  const unwantedTags = options.unwantedTags.join('|');
+  const regexUnwantedTags = unwantedTags ? `(/?${unwantedTags})` : ``;
+  const regex = new RegExp(`(<${regexUnwantedTags}([^>]+)?>)`, 'gi');
+
+  return string.replace(regex, '');
+};
