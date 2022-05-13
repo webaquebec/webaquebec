02a209ab31375b32a08851fc73b9238c679566aa
lornz- <lrt.hrl@gmail.com>
2022-05-13 13:36:23 -0400

    :art: (global) Merge list styles from BlockList to global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -326,7 +326,84 @@ export const unstyledListStyle = css`
   list-style: none;
 `;
 
-export const unorderListStyle = css`
+export const orderedListStyle = css`
+  &,
+  ol {
+    list-style: none;
+
+    counter-reset: li;
+  }
+
+  ol,
+  li:not(:first-of-type) {
+    ${breakpointsRange(
+      [{ prop: 'marginTop', sizes: [14, 16], bases: [16, 20] }],
+      breakpoints.spacings
+    )};
+  }
+
+  li {
+    counter-increment: li;
+  }
+
+  li::before {
+    display: inline-block;
+
+    width: 1.3em;
+    margin-left: -1.3em;
+
+    color: ${colors.bleu80};
+    font-weight: ${fontWeights.bold};
+
+    direction: rtl;
+
+    content: '.' counter(li);
+  }
+
+  li li::before {
+    content: '.' counter(li, lower-alpha);
+  }
+
+  li li li::before {
+    width: 1.8em;
+    margin-left: -1.8em;
+
+    content: '(' counter(li) ')';
+  }
+`;
+
+export const unorderedListStyle = css`
+  &,
+  ul {
+    list-style: none;
+  }
+
+  li {
+    margin-bottom: 16px;
+  }
+
+  li::before {
+    display: inline-block;
+
+    width: 1em;
+    margin-left: -1em;
+
+    color: ${colors.bleu80};
+    font-weight: ${fontWeights.bold};
+
+    content: '•';
+  }
+
+  li li::before {
+    content: '○';
+  }
+
+  li li li::before {
+    content: '-';
+  }
+`;
+
+export const unorderedListStyleAlt = css`
   ${unstyledListStyle};
 
   li {
@@ -346,8 +423,6 @@ export const unorderListStyle = css`
     content: '•';
   }
 `;
-
-export const orderListStyle = css``;
 
 export const resetStyle = css`
   *,
