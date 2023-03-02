92c7a3ac8bc32d733173f6f932bedb82e8f27e21
florianfmmartin <florian.fm.martin@gmail.com>
2023-03-02 16:57:24 -0500

    :speech_balloon: texts: Update logo partern add team forward

--- src/components/PartnersGrids/PartnersGrids.jsx
+++ src/components/PartnersGrids/PartnersGrids.jsx
@@ -36,6 +36,7 @@ import logoPetal from '../../images/logoPartners/associates/logo-petal.svg';
 import logoThirdbridge from '../../images/logoPartners/associates/logo-thirdbridge.svg';
 import logoNventive from '../../images/logoPartners/associates/logo-nventive.svg';
 import logoEdgenda from '../../images/logoPartners/associates/logo-edgenda.svg';
+import logoTeamForward from '../../images/logoPartners/supporters/logo-team-forward.png';
 
 // styles
 import {
@@ -168,6 +169,11 @@ const supportersImages = [
     src: logoZenika,
     alt: 'Zenika | Cabinet d’innovation technologique',
     url: 'https://zenika.com/fr-CA',
+  },
+  {
+    src: logoTeamForward,
+    alt: 'Projet Team Forward | Une nouvelle façon de donner',
+    url: 'https://projetteamforward.org/',
   },
   // {
   //   src: logoBotpress,
