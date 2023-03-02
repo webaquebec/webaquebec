39a8f4ab913984792cd289d0342612109969e413
lornz- <lrt.hrl@gmail.com>
2023-03-02 15:57:03 -0500

    :zap: perf: Render CookieBanner component in client only
    Wrap into ClientOnly HOC avoiding rehydration issue with React

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -14,6 +14,7 @@ import Footer from '../Footer';
 import Header from '../Header';
 import PixelGradient from '../PixelGradient/PixelGradient';
 import CookieBanner from '../CookieBanner';
+import ClientOnly from '../ClientOnly/ClientOnly';
 
 /**
  * The common Layout shared across all pages
@@ -33,7 +34,9 @@ const Layout = ({ children, location }) => {
 
       <PixelGradient pathname={pathname} />
 
-      <CookieBanner location={location} />
+      <ClientOnly>
+        <CookieBanner location={location} />
+      </ClientOnly>
 
       <Header pathname={pathname} />
 
