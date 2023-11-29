c97aa96c9bc6837f52c65735a7579fbbe84d566c
Krystina Hamel <krystina.hamel@libeo.com>
2023-11-28 21:15:07 -0500

    :speech_balloon: texts: Update about pages

--- src/views/AboutEventPageView/AboutEventSection/AboutEventSection.jsx
+++ src/views/AboutEventPageView/AboutEventSection/AboutEventSection.jsx
@@ -1,5 +1,6 @@
 // vendors
 import React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
 import PropTypes from 'prop-types';
 import { hideVisually } from 'polished';
 
@@ -11,28 +12,88 @@ import {
   atRight,
   Container,
   ContentWrapper,
-  SectionParagraph,
+  SectionText,
   SectionPicture,
   SectionTitle,
+  ArchivesList,
+  ArchiveItem,
+  ArchiveLink,
 } from './AboutEventSection.styles';
 
-const AboutEventSection = ({ items }) => (
-  <Container faded>
-    <h3 css={hideVisually}>Points forts de l&apos;événement</h3>
+const AboutEventSection = ({ items }) => {
+  const archives = [
+    {
+      year: '2022',
+      link: '/programmation/2022',
+      type: 'internal',
+    },
+    {
+      year: '2021',
+      link: '/programmation/2021',
+      type: 'internal',
+    },
+    {
+      year: '2020',
+      link: 'https://archives.webaquebec.org/programmation/2020/0',
+    },
+    {
+      year: '2019',
+      link: 'https://archives.webaquebec.org/programmation/2019/0',
+    },
+    {
+      year: '2018',
+      link: 'https://archives.webaquebec.org/programmation/2018/0',
+    },
+    {
+      year: '2017',
+      link: 'https://archives.webaquebec.org/programmation/2017/0',
+    },
+    { year: '2016', link: 'https://2016.webaquebec.org/' },
+    { year: '2015', link: 'https://2015.webaquebec.org/' },
+    { year: '2014', link: 'https://2014.webaquebec.org/' },
+    { year: '2013', link: 'https://2013.webaquebec.org/' },
+    { year: '2012', link: 'https://2012.webaquebec.org/' },
+  ];
 
-    <Center maxWidth='1080px' gutters='16px'>
-      {items.map((item, index) => (
-        <ContentWrapper key={item.id} css={index === 1 && atRight}>
-          <div>
-            <SectionPicture fluid={item.photoSrc} alt={item.title} />
-            <SectionTitle>{item.title}</SectionTitle>
-            <SectionParagraph dangerouslySetInnerHTML={{ __html: item.text }} />
-          </div>
-        </ContentWrapper>
-      ))}
-    </Center>
-  </Container>
-);
+  return (
+    <Container faded>
+      <h3 css={hideVisually}>Points forts de l&apos;événement</h3>
+
+      <Center maxWidth='1080px' gutters='16px'>
+        {items.map((item, index) => (
+          <ContentWrapper key={item.id} css={index === 1 && atRight}>
+            <div>
+              <SectionPicture fluid={item.photoSrc} alt={item.title} />
+              <SectionTitle>{item.title}</SectionTitle>
+              <SectionText dangerouslySetInnerHTML={{ __html: item.text }} />
+              {item.hasArchives && (
+                <ArchivesList>
+                  {archives.map((archive) => (
+                    <ArchiveItem key={archive.year}>
+                      {archive.type && archive.type === 'internal' ? (
+                        <ArchiveLink as={GatsbyLink} to={archive.link}>
+                          {archive.year}
+                        </ArchiveLink>
+                      ) : (
+                        <ArchiveLink
+                          href={archive.link}
+                          rel='noopener noreferrer'
+                          target='_blank'
+                        >
+                          {archive.year}
+                        </ArchiveLink>
+                      )}
+                    </ArchiveItem>
+                  ))}
+                </ArchivesList>
+              )}
+            </div>
+          </ContentWrapper>
+        ))}
+      </Center>
+    </Container>
+  );
+};
 
 AboutEventSection.propTypes = {
   items: PropTypes.arrayOf(
--- src/views/AboutQuebecNumeriquePageView/WhoWeAreSection/WhoWeAreSection.jsx
+++ src/views/AboutQuebecNumeriquePageView/WhoWeAreSection/WhoWeAreSection.jsx
@@ -10,18 +10,16 @@ import { Container, Title, Paragraph } from './WhoWeAreSection.styles';
 const WhoWeAreSection = () => (
   <Center maxWidth='840px' gutters='var(--container-gutter)'>
     <Container>
-      <Title>qui sommes-nous?</Title>
+      <Title>Qui sommes-nous?</Title>
       <Paragraph>
-        Cette grande aventure débuta par le Web à Québec, un événement unique
-        imaginé par des mordus du numérique qui ont eu la folle idée d’organiser
-        un événement pour rassembler les acteurs du Web. La première édition a
-        attiré plus de 200 personnes!
+        Cette grande aventure débuta en 2010 grâce au Web à Québec, un événement
+        unique imaginé par des mordus du numérique qui ont eu la folle idée
+        d’organiser un événement pour rassembler les acteurs du Web. La première
+        édition a attiré plus de 200 personnes!
       </Paragraph>
       <Paragraph>
         Avec le succès grandissant du WAQ, l’organisation se structure
         officiellement. C’est la naissance de l’OBNL Québec numérique.
-        Aujourd’hui encore, QN est au cœur du plus grand événement numérique
-        francophone en Amérique du Nord, créé par et pour la communauté.
       </Paragraph>
     </Container>
   </Center>
