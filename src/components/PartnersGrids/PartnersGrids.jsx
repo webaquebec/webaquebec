f4a695a0cfda528eccda25a8e296d75b80626851
Laurent Harel <lrt.hrl@gmail.com>
2024-02-21 16:15:23 -0500

    :bento: assets: Update partners grid
    Replace nooe logo webp by png version
    
    Add Pangram Pangram as community partner
    
    Remove
    commented code

--- src/components/PartnersGrids/PartnersGrids.jsx
+++ src/components/PartnersGrids/PartnersGrids.jsx
@@ -1,13 +1,9 @@
 // vendors
 import React from 'react';
-// import PropTypes from 'prop-types';
 
 // components
 import LogoCard from './LogoCard';
 import Center from '../LayoutSections/Center';
-// import Button from '../Button';
-// import Switcher from '../LayoutSections/Switcher';
-// import Stack from '../LayoutSections/Stack/Stack';
 
 // images
 import logoUbisoft from '../../images/logoPartners/premium/logo-ubisoft.png';
@@ -16,54 +12,16 @@ import logoFernandez from '../../images/logoPartners/associates/logo-fernandez-r
 import logoDeMarque from '../../images/logoPartners/associates/logo-demarque.svg';
 import logoPetal from '../../images/logoPartners/associates/logo-petal.svg';
 import logoThirdbridge from '../../images/logoPartners/associates/logo-thirdbridge.svg';
-import logoNooe from '../../images/logoPartners/associates/logo-nooe.webp';
+import logoNooe from '../../images/logoPartners/associates/logo-nooe.png';
+import logoPangram from '../../images/logoPartners/associates/logo-pangram-pangram.svg';
 import logoBarberie from '../../images/logoPartners/associates/logo-barberie.png';
 import logoCiao from '../../images/logoPartners/associates/logo-ciao.png';
 import logoBobba from '../../images/logoPartners/associates/logo-bobba.svg';
 import logoEvive from '../../images/logoPartners/associates/logo-evive.png';
 import logoNovicom from '../../images/logoPartners/associates/logo-novicom.png';
-// import logoVideotronRGB from '../../images/logoPartners/premium/logo-videotron-rgb.svg';
-// import logoVideotronWhite from '../../images/logoPartners/premium/logo-videotron-white.svg';
-// import logoQuebecor from '../../images/logoPartners/premium/logo-quebecor.svg';
-// import logoLevio from '../../images/logoPartners/premium/logo-levio.svg';
-// import logoEquisoft from '../../images/logoPartners/premium/logo-equisoft.svg';
-// import logoIxmedia from '../../images/logoPartners/premium/logo-ixmedia.svg';
-// import logoOqlf from '../../images/logoPartners/premium/logo-oqlf.svg';
-// import logoBeet from '../../images/logoPartners/associates/logo-beet.svg';
-// import logoEmergensys from '../../images/logoPartners/associates/logo-emergensys.png';
-// import logoFlots from '../../images/logoPartners/associates/logo-flots.svg';
-// import logoMicrosoft from '../../images/logoPartners/associates/logo-microsoft.svg';
-// import logoPwc from '../../images/logoPartners/associates/logo-pwc.svg';
-// import logoRobic from '../../images/logoPartners/associates/logo-robic.svg';
-// import logoTVA from '../../images/logoPartners/associates/logo-tva.svg';
-// import logoJournalQuebec from '../../images/logoPartners/associates/logo-journal-de-quebec.svg';
-// import logoGrenierAuxNouvelles from '../../images/logoPartners/associates/logo-grenier-aux-nouvelles.svg';
-// import logoZenika from '../../images/logoPartners/supporters/logo-zenika.svg';
-// import logoNventive from '../../images/logoPartners/associates/logo-nventive.svg';
-// import logoEdgenda from '../../images/logoPartners/associates/logo-edgenda.svg';
-// import logoProjetTeamForward from '../../images/logoPartners/supporters/logo-projet-team-forward.svg';
-// import logoDashThis from '../../images/logoPartners/associates/logo-dashthis.svg';
-// import logoRandstad from '../../images/logoPartners/supporters/logo-randstad.svg';
-// import logoCrackmediaRGB from '../../images/logoPartners/supporters/logo-crackmedia-rgb.svg';
-// import logoCrackmediaBlack from '../../images/logoPartners/supporters/logo-crackmedia-black.svg';
-// import logoEvolvingWeb from '../../images/logoPartners/supporters/logo-evolving-web.svg';
-// import logoLibeo from '../../images/logoPartners/supporters/logo-libeo.svg';
-// import logoBotpress from '../../images/logoPartners/supporters/logo-botpress.svg';
-// import logoApollo13 from '../../images/logoPartners/supporters/logo-apollo13.svg';
 
 // styles
-import {
-  Title,
-  // PresentingWrapper,
-  // PresentingText,
-  Grid,
-  GridItem,
-  // premiumGrid,
-  // premiumGridItem,
-  // presentingContentStyle,
-} from './PartnersGrids.styles';
-
-// const presentingImage = { };
+import { Title, Grid, GridItem } from './PartnersGrids.styles';
 
 const creativePartners = [
   {
@@ -112,6 +70,11 @@ const communityPartners = [
     alt: 'Nooé',
     url: 'https://www.nooe.org',
   },
+  {
+    src: logoPangram,
+    alt: 'Pangram Pangram Foundry',
+    url: 'https://pangrampangram.com/',
+  },
 ];
 
 const providerPartners = [
@@ -158,52 +121,6 @@ const partners = [
 
 const PartnersGrids = () => (
   <>
-    {/* <Center
-      as='section'
-      maxWidth='1024px'
-      gutters='var(--container-gutter)'
-      intrinsic
-      withText
-    >
-      <Title>le WAQ 2023 est présenté par</Title>
-
-      <Switcher
-        threshold='768px'
-        space='calc(var(--container-gutter) * 2)'
-        limit={2}
-      >
-        <PresentingWrapper>
-          <div>
-            <LogoCard image={presentingImage} isPresenting />
-          </div>
-
-          {hasDetails && (
-            <div>
-              <Center maxWidth='625px' gutters='0' css={presentingContentStyle}>
-                <PresentingText>
-                  Ubisoft est reconnu internationalement, mais la franchise de
-                  la ville de Québec se démarque particulièrement. Ayant acquis
-                  une solide expertise dans le développement de jeux d’envergure
-                  AAA, le studio assume maintenant le leadership sur plusieurs
-                  projets. Le WAQ est très fier d’être présenté par Ubisoft!
-                </PresentingText>
-                <Button
-                  to='https://www.ubisoft.com/fr-ca/'
-                  tag='href'
-                  rel='noopener noreferrer'
-                  target='_blank'
-                  small
-                  animated
-                >
-                  En savoir plus
-                </Button>
-              </Center>
-            </div>
-          )}
-        </PresentingWrapper>
-      </Switcher>
-    </Center> */}
-
     {partners.map((partner) => (
       <Center
         as='section'
@@ -225,21 +142,5 @@ const PartnersGrids = () => (
     ))}
   </>
 );
-
-PartnersGrids.propTypes = {
-  /**
-   * Whether the grid has details or not.
-   */
-  // hasDetails: PropTypes.bool,
-  /**
-   * Whether the grid has a title or not.
-   */
-  // titled: PropTypes.bool,
-};
-
-PartnersGrids.defaultProps = {
-  // hasDetails: false,
-  // titled: false,
-};
 
 export default PartnersGrids;
