6cb81493bd2788c4e88c5bdc88bb3834fd8d1d73
Krystina Hamel <krystina.hamel@libeo.com>
2024-05-24 07:27:58 -0400

    :bug: fix: Add missing translation to menu

--- src/components/Header/BurgerButton/BurgerButton.jsx
+++ src/components/Header/BurgerButton/BurgerButton.jsx
@@ -2,26 +2,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// components
+import { useTranslation } from 'react-i18next';
+
 // styles
 import { Button, Lines, Line } from './BurgerButton.styles';
 
-const BurgerButton = ({ opened, onClick, ...rest }) => (
-  <Button
-    aria-label={
-      opened ? 'Fermer le menu de navigation' : 'Ouvrir le menu de navigation'
-    }
-    aria-expanded={opened}
-    onClick={onClick}
-    $active={opened}
-    {...rest}
-  >
-    <Lines>
-      <Line />
-      <Line />
-      <Line />
-    </Lines>
-  </Button>
-);
+const BurgerButton = ({ opened, onClick, ...rest }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      aria-label={
+        opened ? t('menu.closeBurgerLabel') : t('menu.openBurgerLabel')
+      }
+      aria-expanded={opened}
+      onClick={onClick}
+      $active={opened}
+      {...rest}
+    >
+      <Lines>
+        <Line />
+        <Line />
+        <Line />
+      </Lines>
+    </Button>
+  );
+};
 
 BurgerButton.propTypes = {
   opened: PropTypes.bool,
